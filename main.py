c71bbea6ceb2e5cef9a2fa4e9c73657479346010
Elysia <1309164618@qq.com>
2026-05-08 21:12:32 +0800

    best.v1

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from ultralytics import  YOLO
 
-model = YOLO("yolo11n.pt")
+model_path = r"C:\Users\YQS\Desktop\yolo\yolo11n.pt"
+model = YOLO(model_path)
 results = model.train(
-    data="",
+    data="./dataset/data.yaml",
     epochs = 50,
     imgsz = 640,
     batch = 8,
+    device = 0,
+    workers = 0,
     patience = 10,
     project = 'runs/temp_seed',
     name = 'v1'
